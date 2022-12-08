97e227be2b7f738656731d767e9520f57529c654
Nam Vu Hoang <nam.vuhoang@vtt.fi>
2022-12-08 21:23:29 +0200

    Check env params.

--- backend/src/environment/environment.ts
+++ backend/src/environment/environment.ts
@@ -1,18 +1,40 @@
 import dotenv from 'dotenv';
+import { env, exit } from 'process';
+import { logger } from '../utils/logger';
 
-dotenv.config({ path: 'env/configuration.env' });
+const configurationFilePath = 'env/configuration.env';
 
-export const environment = {
-  graphqlServer: {
-    port: parseInt(process.env.PORT || '3000'),
-  },
-  dataServer: {
-    baseUrl: process.env.DATA_SERVER_BASE_URL!,
-    clientInfo: {
-      client_id: process.env.DATA_SERVER_CLIENT_ID!,
-      email: process.env.DATA_SERVER_CLIENT_EMAIL!,
-      name: process.env.DATA_SERVER_CLIENT_NAME!,
+function requireConfigParam(name: string) {
+  if (!env[name]) {
+    logger.error(`Param '${name}' is undefined in the configuration file '${configurationFilePath}'.`);
+    logger.info('Application is exiting...');
+    exit();
+  }
+}
+
+function loadConfig() {
+  dotenv.config({ path: configurationFilePath });
+  requireConfigParam('PORT');
+  requireConfigParam('DATA_SERVER_BASE_URL');
+  requireConfigParam('DATA_SERVER_CLIENT_ID');
+  requireConfigParam('DATA_SERVER_CLIENT_EMAIL');
+  requireConfigParam('DATA_SERVER_CLIENT_NAME');
+  requireConfigParam('DATA_POSTS_MAX_PAGE_COUNT');
+
+  return {
+    graphqlServer: {
+      port: parseInt(env.PORT!),
     },
-    pageCount: parseInt(process.env.DATA_POSTS_MAX_PAGE_COUNT!),
-  },
-};
+    dataServer: {
+      baseUrl: env.DATA_SERVER_BASE_URL!,
+      clientInfo: {
+        client_id: env.DATA_SERVER_CLIENT_ID!,
+        email: env.DATA_SERVER_CLIENT_EMAIL!,
+        name: env.DATA_SERVER_CLIENT_NAME!,
+      },
+      pageCount: parseInt(env.DATA_POSTS_MAX_PAGE_COUNT!),
+    },
+  };
+}
+
+export const environment = loadConfig();
