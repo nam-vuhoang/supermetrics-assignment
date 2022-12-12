44f60c68c6630836fb09c84ce0e93fa17393aa1b
Nam Vu Hoang <nam.vuhoang@vtt.fi>
2022-12-12 16:04:27 +0200

    Add PageLayout.

--- posts-frontend/pages/_app.tsx
+++ posts-frontend/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.scss";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
+import type { AppProps } from "next/app";
+import Layout from "../app/Layout";
+import PageLayout from "../app/PageLayout";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <PageLayout>
+      <Component {...pageProps} />
+    </PageLayout>
+  );
 }
--- posts-frontend/pages/test/material.tsx
+++ posts-frontend/pages/test/material.tsx
@@ -1,9 +1,16 @@
-import { Pagination } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
+import { Pagination } from '@mui/material';
 
 function TestMaterial() {
   return (
     <>
-    
+      <Tooltip title="Delete">
+        <IconButton>
+          <DeleteIcon />
+        </IconButton>
+      </Tooltip>
       <Pagination count={10} />
     </>
   );
