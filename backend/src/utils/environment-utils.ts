a483cbcdafadaa91012b194e8d7b2c8e06cfb821
Nam Vu Hoang <nam.vuhoang@vtt.fi>
2022-12-15 02:24:45 +0200

    Remove config file name.

--- backend/src/utils/environment-utils.ts
+++ backend/src/utils/environment-utils.ts
@@ -1,23 +1,21 @@
 import { env } from "process";
 
-export const ENVIRONMENT_FILE_PATH = 'env/configuration.env';
-
 export function requireStringEnvParam(name: string): string {
   if (env[name]) {
     return env[name];
   }
-  throw new EvalError(`Param '${name}' in configuration file '${ENVIRONMENT_FILE_PATH}' is undefined .`);
+  throw new EvalError(`Environment param '${name}' is undefined .`);
 }
 
 export function requireIntEnvParam(name: string): number  {
   const floatValue = Number(requireStringEnvParam(name));
   if (isNaN(floatValue)) {
-    throw new EvalError(`Param '${name}' in configuration file '${ENVIRONMENT_FILE_PATH}' must be integer.`);
+    throw new EvalError(`Environment param '${name}' must be integer.`);
   }  
   
   const intValue = Math.floor(floatValue);
   if (floatValue == intValue) {
     return intValue;
   }
-  throw new EvalError(`Param '${name}' in configuration file '${ENVIRONMENT_FILE_PATH}' must be integer.`);
+  throw new EvalError(`Environment param '${name}' must be integer.`);
 }
