1963103884a5cdb4c8b3827b1b5d422e4f51a241
Nam Vu Hoang <nam.vuhoang@vtt.fi>
2022-12-16 00:43:04 +0200

    Test Environment Utils.

--- backend/src/environment/environment.ts
+++ backend/src/environment/environment.ts
@@ -1,20 +1,20 @@
 import dotenv from 'dotenv';
-import { requireIntEnvParam, requireStringEnvParam } from '../utils/environment-utils';
+import { EnvironmentUtils } from '../utils/environment-utils';
 
 function loadConfig() {
   dotenv.config();
   return {
     graphqlServer: {
-      port: requireIntEnvParam('PORT'),
+      port: EnvironmentUtils.requireIntegerEnvParam('PORT'),
     },
     dataServer: {
-      baseUrl: requireStringEnvParam('DATA_SERVER_BASE_URL'),
+      baseUrl: EnvironmentUtils.requireStringEnvParam('DATA_SERVER_BASE_URL'),
       clientInfo: {
-        client_id: requireStringEnvParam('DATA_SERVER_CLIENT_ID'),
-        email: requireStringEnvParam('DATA_SERVER_CLIENT_EMAIL'),
-        name: requireStringEnvParam('DATA_SERVER_CLIENT_NAME'),
+        client_id: EnvironmentUtils.requireStringEnvParam('DATA_SERVER_CLIENT_ID'),
+        email: EnvironmentUtils.requireStringEnvParam('DATA_SERVER_CLIENT_EMAIL'),
+        name: EnvironmentUtils.requireStringEnvParam('DATA_SERVER_CLIENT_NAME'),
       },
-      pageCount: requireIntEnvParam('DATA_POSTS_MAX_PAGE_COUNT'),
+      pageCount: EnvironmentUtils.requireIntegerEnvParam('DATA_POSTS_MAX_PAGE_COUNT'),
     },
   };
 }
--- backend/src/utils/environment-utils.ts
+++ backend/src/utils/environment-utils.ts
@@ -1,21 +1,24 @@
 import { env } from "process";
 
-export function requireStringEnvParam(name: string): string {
-  if (env[name]) {
-    return env[name];
+export class EnvironmentUtils {
+  static requireStringEnvParam(name: string): string {
+    if (env[name]) {
+      return env[name];
+    }
+    throw new EvalError(`Environment param '${name}' is undefined.`);
   }
-  throw new EvalError(`Environment param '${name}' is undefined .`);
+  
+  static requireIntegerEnvParam(name: string): number  {
+    const floatValue = Number(EnvironmentUtils.requireStringEnvParam(name));
+    if (isNaN(floatValue)) {
+      throw new EvalError(`Environment param '${name}' must be integer.`);
+    }  
+    
+    const intValue = Math.floor(floatValue);
+    if (floatValue == intValue) {
+      return intValue;
+    }
+    throw new EvalError(`Environment param '${name}' must be integer.`);
+  }
 }
 
-export function requireIntEnvParam(name: string): number  {
-  const floatValue = Number(requireStringEnvParam(name));
-  if (isNaN(floatValue)) {
-    throw new EvalError(`Environment param '${name}' must be integer.`);
-  }  
-  
-  const intValue = Math.floor(floatValue);
-  if (floatValue == intValue) {
-    return intValue;
-  }
-  throw new EvalError(`Environment param '${name}' must be integer.`);
-}
