f5a0266812bb50c4eb0799f77f9322891db6898e
Nam Vu Hoang <nam.vuhoang@vtt.fi>
2022-12-17 00:00:06 +0200

    Minor update.

--- posts-frontend/components/page-layout.tsx
+++ posts-frontend/components/page-layout.tsx
@@ -3,28 +3,27 @@ import AppBar from '@mui/material/AppBar';
 import CssBaseline from '@mui/material/CssBaseline';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import {Typography, Link as MaterialLink } from '@mui/material';
+import { Typography, Link as MaterialLink } from '@mui/material';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Diversity1 } from '@mui/icons-material';
 import Head from 'next/head';
 import { Button, IconButton } from '@mui/material';
 import Link from 'next/link';
+import Loading from './loading';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="text.secondary" align="center">
       {'Copyright © '}
-      {/* <Link color="inherit" href="https://supermetrics.com/">
+      <MaterialLink color="inherit" href="https://supermetrics.com/">
         SuperMetrics
-      </Link>{' '} */}
+      </MaterialLink>{' '}
       {new Date().getFullYear()}
       {'.'}
     </Typography>
   );
 }
-
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
 const theme = createTheme();
 
@@ -52,7 +51,7 @@ export default function PageLayout(props: { children: any }) {
                   variant="h6"
                   color="inherit"
                   noWrap
-                  sx={{ flexGrow: 1 }}
+                  sx={{ flexGrow: 1, textDecoration: 'none' }}
                   underline="none"
                 >
                   Heart Book
@@ -71,6 +70,7 @@ export default function PageLayout(props: { children: any }) {
             </Toolbar>
           </AppBar>
         </Box>
+
         {/* Main */}
 
         <main>
@@ -85,19 +85,19 @@ export default function PageLayout(props: { children: any }) {
           </Box>
         </main>
         {/* Footer */}
-        <footer>
-          <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
-            <Typography
-              variant="subtitle1"
-              align="center"
-              color="text.secondary"
-              component="p"
-            >
-              Powered by Nam Vu Hoang
-            </Typography>
-            <Copyright />
-          </Box>
-        </footer>
+        {/* <footer> */}
+        <Box sx={{ bgcolor: 'background.paper', p: 6 }} component="footer">
+          <Typography
+            variant="subtitle1"
+            align="center"
+            color="text.secondary"
+            component="p"
+          >
+            Powered by Nam Vu Hoang
+          </Typography>
+          <Copyright />
+        </Box>
+        {/* </footer> */}
         {/* End footer */}
       </ThemeProvider>
     </>
