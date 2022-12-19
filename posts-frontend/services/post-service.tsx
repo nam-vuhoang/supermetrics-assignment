d10b605c5eeb936f6376544c6db18ce02bb34eed
Nam Vu Hoang <nam.vuhoang@vtt.fi>
2022-12-20 01:08:30 +0200

    Clean code.

--- posts-frontend/services/post-service.tsx
+++ posts-frontend/services/post-service.tsx
@@ -5,10 +5,24 @@ import { User } from '../models/user';
 import { logger } from '../utils/logger';
 import { Utils } from '../utils/utils';
 
+/**
+ * Provides services for fetching data from GraphQL Server.
+ */
 export class PostService {
-  constructor(private backendUrl: string) {}
+  /**
+   * Create the service provider.
+   * @param backendUrl The backend URL is specified every time because environement variable
+   * process.env.XXX is invisible on the client side while using SWR.
+   */
+  constructor(protected backendUrl: string) {}
 
-  private async fetchRawData<T>(
+  /**
+   * Fetch the raw query data. This method can be overwritten by child classes.
+   * @param query
+   * @param variables
+   * @returns
+   */
+  protected async fetchQuery<T>(
     query: string,
     variables?: Variables
   ): Promise<T> {
@@ -17,12 +31,25 @@ export class PostService {
     );
   }
 
+  /**
+   * Fetch the blog data.
+   * @param query
+   * @param variables
+   * @returns
+   */
   private async fetchBlog(query: string, variables?: Variables): Promise<Blog> {
-    return this.fetchRawData<{ blog: Blog }>(query, variables).then(
+    return this.fetchQuery<{ blog: Blog }>(query, variables).then(
       (data) => data.blog
     );
   }
 
+  /**
+   * Fetch all posts with filter.
+   * @param pageIndex Zero-based page index
+   * @param pageSize Page size
+   * @param userId User ID, if null then no user filter will be applied.
+   * @returns
+   */
   async fetchPosts(
     pageIndex: number,
     pageSize: number,
@@ -33,11 +60,7 @@ export class PostService {
       `[GraphQL] Fetching posts with filter ${JSON.stringify(filter)}...`
     );
     const query = gql`
-      query fetchPosts(
-        $pageIndex: Int!
-        $pageSize: Int!
-        $userId: ID
-      ) {
+      query fetchPosts($pageIndex: Int!, $pageSize: Int!, $userId: ID) {
         blog(
           filter: {
             page: { index: $pageIndex, size: $pageSize }
@@ -60,6 +83,10 @@ export class PostService {
     return this.fetchBlog(query, filter);
   }
 
+  /**
+   * Fetch the list of all users. This method is needed for getStaticPaths() in the dashboard page.
+   * @returns
+   */
   async fetchUserIds(): Promise<string[]> {
     logger.debug('[GraphQL] Fetching user IDs...');
     const query = gql`
@@ -71,11 +98,17 @@ export class PostService {
       }
     `;
 
-    return this.fetchRawData<{ users: User[] }>(query).then((data) =>
+    return this.fetchQuery<{ users: User[] }>(query).then((data) =>
       Utils.sortArray(data.users, (u) => u.userName).map((s) => s.userId)
     );
   }
 
+  /**
+   * Fetch full user statistics with user filter. Because the dashboard page always needs statistics
+   * for all users, so this parameter is currently not used anywhere.
+   * @param userId User ID, if null then no user filter will be applied.
+   * @returns
+   */
   async fetchFullStats(userId?: string): Promise<User[]> {
     logger.debug(`[GraphQL] Fetching full stats for user ${userId}...`);
     const query = gql`
