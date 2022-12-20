a0765e4563a2a0c47161a7b1fcfe162a5b25a285
Nam Vu Hoang <nam.vuhoang@vtt.fi>
2022-12-20 16:04:53 +0200

    Minor update.

--- posts-frontend/components/user-list-menu.tsx
+++ posts-frontend/components/user-list-menu.tsx
@@ -5,6 +5,7 @@ import {
   Typography,
   Tooltip,
   TableBody,
+  TableContainer,
 } from '@mui/material';
 import Link from 'next/link';
 import { User } from '../models/user';
@@ -28,39 +29,44 @@ export default function UserListMenu({
   noUserItemTooltip: string;
 }) {
   return (
-    <Table>
-      <TableBody>
-        <IfElseElement
-          if={noUserItemName}
-          then={
-            <TableRow>
-              <TableCell>
-                <Tooltip title={noUserItemTooltip}>
-                  <Link href={href} shallow>
-                    <Typography fontWeight="bold" color="primary">
-                      {noUserItemName}
-                    </Typography>
-                  </Link>
-                </Tooltip>
-              </TableCell>
-            </TableRow>
-          }
-        />
+    <TableContainer>
+      <Table>
+        <TableBody>
+          <IfElseElement
+            if={noUserItemName}
+            then={
+              <TableRow>
+                <TableCell>
+                  <Tooltip title={noUserItemTooltip}>
+                    <Link href={href} shallow>
+                      <Typography fontWeight="bold" color="primary">
+                        {noUserItemName}
+                      </Typography>
+                    </Link>
+                  </Tooltip>
+                </TableCell>
+              </TableRow>
+            }
+          />
 
-        <TableRow>
-          <TableCell sx={{ borderBottomWidth: noUserItemName && 0 }}>
-            {users.map((u) => (
-              <div key={u.userId}>
-                <Tooltip title={itemTooltip(u.userName)}>
-                  <Link href={`${href}${encodeURIComponent(u.userId)}`} shallow>
-                    <Typography color="primary">{u.userName}</Typography>
-                  </Link>
-                </Tooltip>
-              </div>
-            ))}
-          </TableCell>
-        </TableRow>
-      </TableBody>
-    </Table>
+          <TableRow>
+            <TableCell sx={{ borderBottomWidth: noUserItemName && 0 }}>
+              {users.map((u) => (
+                <div key={u.userId}>
+                  <Tooltip title={itemTooltip(u.userName)}>
+                    <Link
+                      href={`${href}${encodeURIComponent(u.userId)}`}
+                      shallow
+                    >
+                      <Typography color="primary">{u.userName}</Typography>
+                    </Link>
+                  </Tooltip>
+                </div>
+              ))}
+            </TableCell>
+          </TableRow>
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
